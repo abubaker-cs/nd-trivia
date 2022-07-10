0d11dd3aadfeca29cf255be368d4af945a177b95
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-07-11 04:13:49 +0500

    Implementing method for sharing success results.

--- app/src/main/java/com/example/android/navigation/ui/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/ui/GameWonFragment.kt
@@ -16,13 +16,15 @@
 
 package com.example.android.navigation.ui
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.findNavController
 import com.example.android.navigation.R
 import com.example.android.navigation.databinding.FragmentGameWonBinding
@@ -52,4 +54,67 @@ class GameWonFragment : Fragment() {
 
         return binding.root
     }
+
+    // Starting an Activity with our new Intent
+    private fun shareSuccess() {
+
+        startActivity(getShareIntent())
+
+    }
+
+    // Creating our Share Intent
+    private fun getShareIntent(): Intent {
+
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+
+        val shareIntent = Intent(Intent.ACTION_SEND)
+
+        shareIntent.setType("text/plain")
+            .putExtra(
+                Intent.EXTRA_TEXT,
+                getString(R.string.share_success_text, args.numCorrect, args.numQuestions)
+            )
+
+        return shareIntent
+
+    }
+
+    // https://developer.android.com/jetpack/androidx/releases/activity#1.4.0-alpha01
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        // The usage of an interface lets you inject your own implementation
+        val menuHost: MenuHost = requireActivity()
+
+        // Add menu items without using the Fragment Menu APIs
+        // Note how we can tie the MenuProvider to the viewLifecycleOwner
+        // and an optional Lifecycle.State (here, RESUMED) to indicate when
+        // the menu should be visible
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menuInflater.inflate(R.menu.winner_menu, menu)
+
+                // check if the activity resolves
+                if (null == getShareIntent().resolveActivity(requireActivity().packageManager)) {
+                    // hide the menu item if it doesn't resolve
+                    menu.findItem(R.id.share)?.isVisible = false
+                }
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Handle the menu selection
+                // Reference: https://developer.android.com/guide/navigation/navigation-navigate#id
+                // view.findNavController().navigate(R.id.aboutFragment)
+
+                when (menuItem.itemId) {
+                    R.id.share -> shareSuccess()
+                }
+
+                return true
+            }
+
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+    }
+
 }
